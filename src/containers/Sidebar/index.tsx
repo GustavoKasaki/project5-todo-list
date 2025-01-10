174ea309550dba051ac7a9edebbf42bf81b6b7b6
GustavoKasaki <gmkasaki@gmail.com>
2025-01-09 21:16:28 -0300

    {feat} added search filter

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,17 +1,30 @@
+import { useDispatch, useSelector } from 'react-redux'
 import CardFilter from '../../components/CardFilter'
 import * as S from './styles'
+import { RootReducer } from '../../store'
+import { changeTerm } from '../../store/reducers/filter'
 
-const Sidebar = () => (
-  <S.Aside>
-    <S.Input type="text" placeholder="Search" />
-    <S.Filter>
-      <CardFilter counter={3} subtitle="pending" />
-      <CardFilter counter={2} subtitle="done" />
-      <CardFilter counter={5} subtitle="urgent" />
-      <CardFilter counter={1} subtitle="important" />
-      <CardFilter counter={1} subtitle="normal" />
-      <CardFilter active counter={12} subtitle="all" />
-    </S.Filter>
-  </S.Aside>
-)
+const Sidebar = () => {
+  const dispatch = useDispatch()
+  const { term } = useSelector((state: RootReducer) => state.filter)
+
+  return (
+    <S.Aside>
+      <S.Input
+        type="text"
+        placeholder="Search"
+        value={term}
+        onChange={(e) => dispatch(changeTerm(e.target.value))}
+      />
+      <S.Filter>
+        <CardFilter counter={3} subtitle="pending" />
+        <CardFilter counter={2} subtitle="done" />
+        <CardFilter counter={5} subtitle="urgent" />
+        <CardFilter counter={1} subtitle="important" />
+        <CardFilter counter={1} subtitle="normal" />
+        <CardFilter active counter={12} subtitle="all" />
+      </S.Filter>
+    </S.Aside>
+  )
+}
 export default Sidebar
--- src/containers/TodoList/index.tsx
+++ src/containers/TodoList/index.tsx
@@ -6,12 +6,19 @@ import { RootReducer } from '../../store'
 
 const TodoList = () => {
   const { items } = useSelector((state: RootReducer) => state.tasks)
+  const { term } = useSelector((state: RootReducer) => state.filter)
+
+  const taskFilter = () => {
+    return items.filter(
+      (item) => item.title.toLowerCase().search(term.toLowerCase()) >= 0
+    )
+  }
 
   return (
     <Container>
-      <p>2 tasks marked as: &quot;category&quot; and &quot;term&quot;</p>
+      <p>2 tasks marked as: &quot;category&quot; and &quot;{term}&quot;</p>
       <ul>
-        {items.map((t) => (
+        {taskFilter().map((t) => (
           <li key={t.title}>
             <Task
               id={t.id}
